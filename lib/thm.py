473d5b962364d372b707c4149cea4ed37b8ac142
AlienX <64488123+AlienX2001@users.noreply.github.com>
2020-11-14 14:14:19 +0530

    Update thm.py

--- lib/thm.py
+++ lib/thm.py
@@ -1,1 +1,28 @@
+import requests
+from os import path
+import json
 
+URL = "https://tryhackme.com/tokens/discord/"
+
+if path.exists("users.txt"):
+    with open("users.txt", "r") as f:
+        users = json.loads(f.read())
+else:
+    users = []
+    
+def verify_user(discord_id,token):
+    re = requests.get(url+token)
+    info = re.json()
+    if(info['success']==True):
+      for user in users:
+            if(user["discord_id"]==discord_id):
+                return "already_in"
+            users.append({
+                "discord_id": discord_id,
+                "id": info['username'],
+            })
+            with open("users.txt", "w") as f:
+                f.write(json.dumps(users))
+            return "accepted"
+    else:
+      return "wrong_id"
